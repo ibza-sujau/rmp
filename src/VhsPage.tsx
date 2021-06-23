11ace2c6c6b4405c9974a239a2841159e37ca61a
Ibza <ibza.sujau@gmail.com>
2021-06-23 14:23:33 +0500

    Fixed RMP buttons.

--- src/VhsPage.tsx
+++ src/VhsPage.tsx
@@ -6,14 +6,14 @@ const VhsPage = () => {
     return (
         <div className={"page"}>
             <div className={"rmp"}>
-                <div>
-                    <MainButton y={3} children={"VHF"} />
-                    <MainButton y={3} children={"HF"} />
-                    <MainButton y={3} children={"TEL"} />
-                    <MainButton y={3} children={"SQWK"} />
-                    <MainButton y={3} children={"--"} />
-                    <MainButton y={3} children={"MENU"} />
-                    <MainButton y={3} children={"NAV"} />
+                <div className={"btn-group"}>
+                    <MainButton children={"VHF"} />
+                    <MainButton children={"HF"} />
+                    <MainButton children={"TEL"} />
+                    <MainButton children={"SQWK"} />
+                    <MainButton children={"--"} />
+                    <MainButton children={"MENU"} />
+                    <MainButton children={"NAV"} />
                 </div>
             </div>
         </div>
--- src/components/buttons/MainButton.tsx
+++ src/components/buttons/MainButton.tsx
@@ -4,14 +4,12 @@ import "./MainButton.scss";
 type MainButtonProps = {
     className?: string
     onClick?: () => void,
-    x?: number,
-    y?: number,
     children?: any,
 }
 
 const MainButton = ({...props}: MainButtonProps) => {
     return(
-        <button className={"main-btn"} onClick={props.onClick} style={{left: props.x + "%", top: props.y + "%"}}>{props.children}</button>
+        <button className={"btn"} onClick={props.onClick}>{props.children}</button>
     )
 }
 
