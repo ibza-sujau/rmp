da678d08e7e6492dbd9cfa01fb80b344009eb83e
Ibza <ibza.sujau@gmail.com>
2021-06-25 10:49:17 +0500

    Created SQWK page, and added basic states.

--- src/pages/SqwkPage.tsx
+++ src/pages/SqwkPage.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
 import './SqwkPage.scss';
+import {useHistory} from "react-router-dom";
+import {useState} from "react";
 
-const SqwkPage = () => {
+// COMPONENTS
+import {Input} from "../components/Input"
+import {Button, ButtonType} from "../components/Button";
+
+export const SqwkPage = () => {
+
+    const history = useHistory();
+
+    const [SQWK, setSQWK] = useState<string>("7000");
+
     return (
         <div className={"page"}>
+            <div className={"flex-row"}>
+                <Button type={ButtonType.Main} children={"VHF"} onClick={() => history.push("/vhs")}/>
+                <Button type={ButtonType.Main} children={"HF"}/>
+                <Button type={ButtonType.Main} children={"TEL"}/>
+                <Button type={ButtonType.Main} children={"SQWK"} onClick={() => history.push("/sqwk")}/>
+                <Button type={ButtonType.Main} children={"-"}/>
+                <Button type={ButtonType.Main} children={"MENU"}/>
+                <Button type={ButtonType.Main} children={"NAV"}/>
+            </div>
+            <div className={"flex-column"}>
+                <div className={"flex-column-item-1"}>
+                    <Button type={ButtonType.Left} children={"⬤"}/>
+                    <Button type={ButtonType.Left} children={"⬤"}/>
+                    <Button type={ButtonType.Left} children={"⬤"}/>
+                    <Button type={ButtonType.Right} children={"MSG\nCLR"}/>
+                </div>
+                <div className={"screen"}>
+                    <div className={"flex-column"}>
+                        <div className={"flex-column-item-1"}>
+                            <p>AUTO</p>
+                        </div>
+                        <div className={"flex-column-item-2"}>
+                            <p>AUTO</p>
+                            <Input type={"text"} value={} placeholder={} characterLimit={}
+                        </div>
+                    </div>
+                </div>
+                <div className={"flex-column-item-2"}>
+                    <Button type={ButtonType.Right} children={"▬"}/>
+                    <Button type={ButtonType.Right} children={"▬"}/>
+                    <Button type={ButtonType.Right} children={"▬"}/>
+                    <Button type={ButtonType.Right} children={"RST"} className={"alt-btn"}/>
+                </div>
+            </div>
+
+
+
+            <div className={"flex-row"}>
+                <Button type={ButtonType.Main} children={"≡≡≡\nCALL"} className={"alt-btn "}/>
+                <Button type={ButtonType.Main} children={"≡≡≡\nCALL"} className={"alt-btn"}/>
+                <Button type={ButtonType.Main} children={"≡≡≡\nCALL"} className={"alt-btn"}/>
+            </div>
+            <div className={"flex-row"}>
+                <Button type={ButtonType.Main} children={"≡≡≡\nCALL"} className={"alt-btn"}/>
+                <Button type={ButtonType.Main} children={"≡≡≡\nCALL"} className={"alt-btn"}/>
+                <Button type={ButtonType.Main} children={"≡≡≡\nCALL"} className={"alt-btn"}/>
+            </div>
         </div>
     )
 }
