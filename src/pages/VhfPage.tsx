478bd6f96b96ae1c13ab025f2826816f5075bd84
Ibza <ibza.sujau@gmail.com>
2021-06-28 22:29:12 +0500

    Added custom local storage hook.

--- src/pages/VhfPage.tsx
+++ src/pages/VhfPage.tsx
@@ -8,33 +8,21 @@ import {Button, ButtonType} from "../components/Button";
 
 // CUSTOM LOCAL STORAGE HOOK
 function useLocalStorage<T>(key: string, initialValue: T) {
-    // State to store our value
-    // Pass initial state function to useState so logic is only executed once
     const [storedValue, setStoredValue] = useState<T>(() => {
         try {
-            // Get from local storage by key
             const item = window.localStorage.getItem(key);
-            // Parse stored json or if none return initialValue
             return item ? JSON.parse(item) : initialValue;
         } catch (error) {
-            // If error also return initialValue
             console.log(error);
             return initialValue;
         }
     });
-    // Return a wrapped version of useState's setter function that ...
-    // ... persists the new value to localStorage.
     const setValue = (value: T | ((val: T) => T)) => {
         try {
-            // Allow value to be a function so we have same API as useState
-            const valueToStore =
-                value instanceof Function ? value(storedValue) : value;
-            // Save state
+            const valueToStore = value instanceof Function ? value(storedValue) : value;
             setStoredValue(valueToStore);
-            // Save to local storage
             window.localStorage.setItem(key, JSON.stringify(valueToStore));
         } catch (error) {
-            // A more advanced implementation would handle the error case
             console.log(error);
         }
     };
@@ -45,12 +33,12 @@ export const VhfPage = () => {
 
     const history = useHistory();
 
-    const [VHF1, setVHF1] = useLocalStorage("VHF1", 0);
-    const [VHF2, setVHF2] = useState<number>(0);
-    const [VHF3, setVHF3] = useState<number>(0);
-    const [STBY1, setSTBY1] = useState<number>(0);
-    const [STBY2, setSTBY2] = useState<number>(0);
-    const [STBY3, setSTBY3] = useState<number>(0);
+    const [VHF1, setVHF1] = useLocalStorage<number>("VHF1", 0);
+    const [VHF2, setVHF2] = useLocalStorage<number>("VHF2", 0);
+    const [VHF3, setVHF3] = useLocalStorage<number>("VHF2", 0);
+    const [STBY1, setSTBY1] = useLocalStorage<number>("VHF2", 0);
+    const [STBY2, setSTBY2] = useLocalStorage<number>("VHF2", 0);
+    const [STBY3, setSTBY3] = useLocalStorage<number>("VHF2", 0);
 
     const [currentVFH, setCurrentVHF] = useState<"VHF1" | "VHF2">("VHF1");
 
